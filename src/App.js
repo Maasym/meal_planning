01f5fe10b24fde67a9eb01227f08b73455a7f27f
maasym <github1@go-nihon.com>
2022-04-11 14:28:19 +0200

    api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
-
+import React, {useState, useEffect} from "react";
+import axios from "axios";
 
 function App() {
+  const [mealData, setMealData] = useState(null);
+  const [calories, setCalories] = useState(2000);
+
+  const url = `https://api.spoonacular.com/mealplanner/generate?apiKey=cb1c464d94f142c08b156c5beddade8b&timeFrame=day&targetCalories=${calories}`
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setMealData(response.data)
+      console.log(response.data)
+    }).catch((error) => {
+      console.log(error)
+    })
+  }, [])
+
+  function handleChange(e) {
+    setCalories(e.target.value);
+  }
+
+
   return (
     <div className="App">
 
